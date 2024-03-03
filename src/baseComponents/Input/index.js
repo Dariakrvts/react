1cd000addf474489d505c090c3afededaa847508
dariakrvts <kravecdasa228@gmail.com>
2024-03-03 21:01:36 +0200

    урок 31. не работают почему то инпуты

--- src/baseComponents/Input/index.js
+++ src/baseComponents/Input/index.js
@@ -1,16 +1,16 @@
 export const Input = ({ handleChange, value, error, type, height, styles }) => {
-  return (
-      <div>
-          {value.error && <span>{value.error}</span>}
+    return (
+        <div>
+            {value.error && <span>{value.error}</span>}
 
-          <input
-              onChange={handleChange}
-              value={value}
-              className={type === "primary" ? "red" : "blue"}
-              height={height}
-              type="text"
-              style={{ ...styles }}
-          />
-      </div>
-  );
+            <input
+                onChange={handleChange}
+                value={value}
+                className={type === "primary" ? "red" : "blue"}
+                height={height}
+                type="text"
+                style={{ ...styles }}
+            />
+        </div>
+    );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,27 @@
-import { useState, useEffect } from "react"
-import {validateUserName} from '../../utils/index'
-
+import { useState, useEffect } from "react";
+import { validateUserName } from "../../utils/index";
+import Input from "baseComponents/Input";
 
 const Home = () => {
-  const [userName, setUserName] = useState ({
-    value: '',
-    error: ''
-  })
+  const [userName, setUserName] = useState({
+    // value: "",
+    error: "",
+  });
 
-  const handleSubmot = () =>{
-    if(validateUserName(userName.value)) {
+  const handleChangeUserName = (e) => {
+    setUserName((userName) => ({...userName, value: e.target.value}))
+  }
+  const handleSubmit = () => {
+    if (!validateUserName(userName.value)) {
       console.log("Validate");
     }
-  }
+  };
 
   return (
-    <div>HomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHome</div>
-  )
+    <div>
+      <Input value={userName.value} handleChange={handleChangeUserName}/>
+    </div>
+  );
+};
 
-}
-
-export default Home
+export default Home;
